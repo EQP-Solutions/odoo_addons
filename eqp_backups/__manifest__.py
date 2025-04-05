6da9a42592e6bd1b77c0610f12c5a94b637a5888
Esteban Quevedo <esteban.quevedo@eqpsolutions.com>
2025-04-05 16:12:05 -0400

    [FIX] (eqp_backups) Image Name Typo.

--- eqp_backups/__manifest__.py
+++ eqp_backups/__manifest__.py
@@ -14,7 +14,7 @@
     "version": "18.0.1.0",
     "category": "Tools",
     "license": "LGPL-3",
-    "images": ["static/description/eqp_backups.gif"],
+    "images": ["static/description/eqp_backup.gif"],
     "author": "EQP Solutions",
     "website": "https://www.eqpsolutions.com/",
     "contributors": [
